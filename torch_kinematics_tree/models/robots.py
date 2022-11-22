01baaf407848dea2f108ccaa1aa04880ef6667af
anindex <an@robot-learning.de>
2022-11-22 17:33:44 +0100

    Remove redundancy
    Signed-off-by: anindex <an@robot-learning.de>

--- torch_kinematics_tree/models/robots.py
+++ torch_kinematics_tree/models/robots.py
@@ -7,7 +7,6 @@ class DifferentiableKUKAiiwa(DifferentiableTree):
     def __init__(self, link_list: Optional[str] = None, device='cpu'):
         robot_file = get_robot_path() / 'kuka_iiwa' / 'urdf' / 'iiwa7.urdf'
         self.model_path = robot_file.as_posix()
-        self.learnable_rigid_body_config = None
         self.name = "differentiable_kuka_iiwa"
         super().__init__(self.model_path, self.name, link_list=link_list, device=device)
 
@@ -19,7 +18,6 @@ class DifferentiableFrankaPanda(DifferentiableTree):
         else:
             robot_file = get_robot_path() / 'franka_description' / 'robots' / 'panda_no_gripper.urdf'
         self.model_path = robot_file.as_posix()
-        self.learnable_rigid_body_config = None
         self.name = "differentiable_franka_panda"
         super().__init__(self.model_path, self.name, link_list=link_list, device=device)
 
@@ -32,7 +30,6 @@ class DifferentiableUR10(DifferentiableTree):
         else:
             robot_file = robot_path / 'ur10' / 'urdf' / 'ur10.urdf'
         self.model_path = robot_file.as_posix()
-        self.learnable_rigid_body_config = None
         self.name = "differentiable_ur10"
         super().__init__(self.model_path, self.name, link_list=link_list, device=device)
 
@@ -41,7 +38,6 @@ class DifferentiableTiagoDualHolo(DifferentiableTree):
     def __init__(self, link_list: Optional[str] = None, device='cpu'):
         robot_file = get_robot_path() / 'tiago_dual_description' / 'tiago_dual_holobase_minimal.urdf'
         self.model_path = robot_file.as_posix()
-        self.learnable_rigid_body_config = None
         self.name = "differentiable_tiago_dual_holo"
         super().__init__(self.model_path, self.name, link_list=link_list, device=device)
 
@@ -50,7 +46,6 @@ class DifferentiableShadowHand(DifferentiableTree):
     def __init__(self, link_list: Optional[str] = None, device='cpu'):
         robot_file = get_robot_path() / 'shadow_hand' / 'shadow_hand.urdf'
         self.model_path = robot_file.as_posix()
-        self.learnable_rigid_body_config = None
         self.name = "differentiable_shadow_hand"
         super().__init__(self.model_path, self.name, link_list=link_list, device=device)
 
@@ -59,6 +54,5 @@ class DifferentiableAllegroHand(DifferentiableTree):
     def __init__(self, link_list: Optional[str] = None, device='cpu'):
         robot_file = get_robot_path() / 'allegro_hand' / 'allegro_hand.urdf'
         self.model_path = robot_file.as_posix()
-        self.learnable_rigid_body_config = None
         self.name = "differentiable_allegro_hand"
         super().__init__(self.model_path, self.name, link_list=link_list, device=device)
